beccc8fa6d753be587f97a6a2c14c9f2c0c18e53
vikranthsb <35425981+vikranthgaddam@users.noreply.github.com>
2023-11-13 03:18:18 -0500

    Working P,PI,PID controller

--- feedback.py
+++ feedback.py
@@ -1,5 +1,6 @@
+
 import math
-import random
+import numpy as np
 
 SamplingInterval = None # Sampling interval - defaults to None, must be set explicitly
 
@@ -9,8 +10,49 @@ SamplingInterval = None # Sampling interval - defaults to None, must be set expl
 '''
     Include your controller(s) code here
 '''
+class PController:
+    def __init__(self, goal, p=2):
+        self.p = p  # Proportional gain
+        self.goal = goal  # Desired goal (setpoint)
 
+    def next(self, output):
+        error = self.goal - output  # Calculate the error
+        control_signal = self.p * error  # Proportional control signal
+        return control_signal
 
+class PIController:
+    def __init__(self, goal, p=1, i=1, dt=0.1):
+        self.p = p
+        self.i = i
+        self.dt = dt
+        self.goal = goal
+        self.integral = 0
+
+    def next(self, output):
+        error = self.goal - output
+        self.integral += error * self.dt
+        control_signal = self.p * error + self.i * self.integral
+        return control_signal
+
+class PIDController:
+    def __init__(self, goal, p=1, i=1, d=0, dt=0.1):
+        self.p = p
+        self.i = i
+        self.d = d
+        self.dt = dt
+        self.goal = goal
+        self.integral = 0
+        self.last_error = 0
+
+    def next(self, output):
+        error = self.goal - output
+        self.integral += error * self.dt
+        derivative = (error - self.last_error) / self.dt
+        
+        control_signal = (self.p * error) + (self.i * self.integral) + (self.d * derivative)
+        
+        self.last_error = error
+        return control_signal
 
 # ============================================================
 # # Input signals
@@ -23,24 +65,72 @@ SamplingInterval = None # Sampling interval - defaults to None, must be set expl
 
 # # ============================================================
 # # Loop functions
+# a, b ,rmse,r_squared= fb.static_test(ServerPool, (0, complete_work, generate_work),
+                 #   40, 40, 5, 5000) # max u, steps, trials, timesteps
 
-def static_test( target_ctor, ctor_args, umax, steps, repeats, tmax ):
-    # Complete test for static process characteristic
-    print("Inside FB ST")
-    for i in range( 0, steps ):
-        u = float(i)*umax/float(steps)
+def static_test(target_ctor, ctor_args, umax, steps, repeats, tmax):
+    u_values = []  # Array for u values
+    averaged_y_values = []  # Array for averaged y values
 
-        for r in range( repeats ):
+    for i in range(steps):
+        u = float(i) * umax / float(steps)
+        print("u values",u)
+       # angle=(math.pi*2*i)/steps
+       # u=(math.sin(angle)+1)*0.5*umax
+        sum_y = 0.0
+
+        for r in range(repeats):
             target = target_ctor(*ctor_args)
-
-            for t in range( tmax ):
+            for t in range(tmax):
                 y = target.work(u)
+            sum_y += y
+        
+        avg_y = sum_y / repeats
 
-            print(u, y)
-                        
-    quit()
+        u_values.append(u)
+        averaged_y_values.append(avg_y)
+
+        if avg_y >= 1.0:
+            break  # Stop if y reaches or exceeds 1
+
+    return parameter_estimation(u_values, averaged_y_values)
+def parameter_estimation(u_values, averaged_y_values):
+   
+    u_values = np.array(u_values)
+    averaged_y_values = np.array(averaged_y_values)
+
+    mu_u = np.mean(u_values)
+    mu_y = np.mean(averaged_y_values)
+
+    
+    u = u_values - mu_u
+    y = averaged_y_values - mu_y
+    print("Y values" ,y)
+    print("U values",u)
 
 
+    
+    S1 = np.sum(y ** 2)
+    S2 = np.sum(u * y)
+    S3 = np.sum(u ** 2)
+    S4 = np.sum(y[:-1] * y[1:])
+    S5 = np.sum(u[:-1] * y[1:])
+
+   
+    denominator = S1 * S3 - S2 ** 2
+    a = (S3 * S4 - S2 * S5) / denominator
+    b = (S1 * S5 - S2 * S4) / denominator
+    #y(k+1)= averaged_y_values
+    y_next_values = [a * y[i] + b * u[i] for i in range(len(y) - 1)]
+
+    rmse = math.sqrt(sum((y[i] - y_next_values[i]) ** 2 for i in range(len(y_next_values))) / len(y_next_values))
+    
+    # Calculate R^2 value
+    total_sum_of_squares = sum((y_i - mu_y) ** 2 for y_i in y[:-1])
+    sum_of_squares_of_residuals = sum((y[i] - y_next_values[i]) ** 2 for i in range(len(y_next_values)))
+    r_squared = 1 - (sum_of_squares_of_residuals / total_sum_of_squares)
+
+    return  a, b ,rmse ,r_squared
 def closed_loop():
     '''
         Your feedback loop code goes here
--- server.py
+++ server.py
@@ -1,119 +1,129 @@
-import math
 import random
+import matplotlib.pyplot as plt
+from feedback import PController, PIController, PIDController
 
 '''
     Feedback structure code. You can use multiple source-codes for each of the controllers.
     Import all the source-codes in this code
 '''
-import feedback as fb 
 
-class AbstractServerPool( fb.Component ):
-    def __init__( self, n, server, client ):
+
+class AbstractServerPool:
+    def __init__(self, n, server, client):
         self.n = n           # number of server instances to start with
-        self.n_requests = 0       # number of arriving requests
-
+        self.n_requests = 0  # number of arriving requests
         self.server = server # server work function
-        self.client = client     # queue-loading work function
+        self.client = client # queue-loading work function
 
-
-    def work( self, u ):
+    def work(self, u):
         self.n = max(0, int(round(u))) # server count: non-negative integer
-
         completed = 0
         for _ in range(self.n):
             completed += self.server() # each server does some amount of work
-
             if completed >= self.n_requests:
-                completed = self.n_requests # "trim" completed to total requests arrived
-                break                  # stop if queue is empty 
-
-        self.n_requests -= completed        # reduce total requests by work completed
-
+                completed = self.n_requests
+                break
+        self.n_requests -= completed
         return completed
 
+    def monitoring(self):
+        # Dummy implementation; replace with actual completion rate retrieval
+        return random.random()
 
-    def monitoring( self ):
-        return "To be implemented"
-
-# ============================================================
 # Server Pool
-class ServerPool( AbstractServerPool ):
-    def work( self, u ):
-        load = self.client()        # generate new requests 
-        self.n_requests = load         # new load replaces old load
+class ServerPool(AbstractServerPool):
+    def work(self, u):
+        load = self.client()
+        self.n_requests = load
+        if load == 0:
+            return 1
+        completed = super().work(u)
+        return completed/load
 
-        if load == 0: return 1    # no work: 100 percent completion rate 
-
-        completed = AbstractServerPool.work( self, u )
-
-        return completed/load     # completion rate
-
-# Generate and Complete functions
-def generate_work():    
+def generate_work():
     global global_time
     global_time += 1
-
     if global_time > 2500:
         return random.gauss(1600, 10)
-
     if global_time > 2200:
         return random.gauss(1000, 10)
-
     return random.gauss(1300, 10)
 
 def complete_work():
     a, b = 20, 3
-    return 100*random.betavariate(a, b) # mean: a/(a+b); var: ~b/a^2
+    return 100*random.betavariate(a, b)
 
-# ============================================================
-'''
-    Method to model the target system
-'''
-def static_test( traffic ):
-    def generate_work():
-        return random.gauss( traffic, traffic/200 )
+# Closed loop with PID
+def closed_loop(target_completion_rate, num_servers, num_iterations,controller_type):
+    server_pool = ServerPool(num_servers, complete_work, generate_work)
+    completion_rates = []
+    control_inputs = []
+    server_counts = []
+    #PI Controller Initialization
+    controller = PController(goal=target_completion_rate,p=3 )
+    piController = PIController(goal=target_completion_rate, p=0.6 , i=0.9, dt=1)
+    pid_controller = PIDController(goal=target_completion_rate, p=0.31, i=1.01, d=0.23, dt=1)
+    current_output=0
+
+    for _ in range(num_iterations):
+        if controller_type == 'p':
+            control_signal = controller.next(current_output)
+        elif controller_type == 'pi':
+            control_signal = piController.next(current_output)
+        elif controller_type == 'pid':
+            control_signal = pid_controller.next(current_output)
+        else:
+            raise ValueError("Invalid controller type specified")
+
+        current_output=server_pool.work(control_signal)
+
+        completion_rates.append(current_output)
+        control_inputs.append(control_signal)
+        server_counts.append(server_pool.n)
+
+    return completion_rates, control_inputs, server_counts
+
+# Plotting function
+def plot_data(completion_rates, control_inputs, server_counts):
+    time_steps = list(range(len(completion_rates)))
     
-    fb.static_test(ServerPool, (0, complete_work, generate_work),
-                    20, 20, 5, 1000) # max u, steps, trials, timesteps
+    fig, axs = plt.subplots(3, 1, figsize=(10, 15))
+    axs[0].plot(time_steps, completion_rates, label='Completion Rate')
+    axs[1].plot(time_steps, control_inputs, label='Control Input', color='orange')
+    axs[2].plot(time_steps, server_counts, label='Server Instances', color='green')
 
-'''
-    Method to simulate the feedback-based system
-'''
-def closed_loop(n):
-    initial_num_servers = n
-    target_system = ServerPool(initial_num_servers, complete_work, generate_work)
-    # controller = Create controller instance
-    # Invoke closed_loop method in the controller
-
-
-# ============================================================
-#Helper Methods
-def plotter():
-    # Your plotter code should go here
-    return
-
-# If you are using any other helper methods, include them here
-
-# ============================================================
-
-
+    for ax in axs:
+        ax.set_xlabel('Time Steps')
+        ax.grid(True)
+        ax.legend()
+    axs[0].set_ylabel('Completion Rate')
+    axs[1].set_ylabel('Control Input')
+    axs[2].set_ylabel('Server Instances')
+    axs[0].set_title('Completion Rate vs. Time Steps')
+    axs[1].set_title('Control Input vs. Time Steps')
+    axs[2].set_title('Server Instances vs. Time Steps')
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == '__main__':
-
     '''
-        TA will only type "python server.py k N T C"
-        
-        k - Number of time steps to simulate
-        N - Number of initial server instances
-        T - Type of test (s, c) - s = static test, c = simulate feedback-based system
-        C - Controller to run (p, pi, pid)
-        p = proportional controller; pi = proportional-integral controller; pid = proportional-integral-derivative controller
-        Note: You must handle any errors in the user input        
-    '''
+TA will only type "python server.py k N T C"
+k - Number of time steps to simulate
+N - Number of initial server instances
+T - Type of test (s, c) - s = static test, c = simulate feedback-based
+system
+C - Controller to run (p, pi, pid)
+p = proportional controller; pi = proportional-integral controller; pid =
+proportional-integral-derivative controller
+Note: You must handle any errors in the user input
+'''
 
-    fb.DT = 1 # Sampling time is set to 1 - Refer to feedback.py
+    
+    global_time = 0  # Global time for generate_work function
+    controller_type = "p"  # Can be 'p', 'pi', or 'pid'
+    target_completion_rate = 0.9
+    num_iterations = 5000
+    initial_num_servers = 10
 
-    global_time = 0 # To communicate with generate and consume functions
-
-    static_test(1000)
-#    closed_loop()
+    completion_rates, control_inputs, server_counts = closed_loop( target_completion_rate, initial_num_servers, num_iterations,controller_type)
+    plot_data(completion_rates, control_inputs, server_counts)
